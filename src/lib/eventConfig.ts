54648731fa70e1d03b8f3cf120c02eebb6e425f5
Nerdwork <nerdworktecnologia@gmail.com>
2026-04-06 22:58:31 -0300

    Atualizar local do evento e ajustar fallback do fundo inicial

--- src/lib/eventConfig.ts
+++ src/lib/eventConfig.ts
@@ -1,9 +1,10 @@
 export const EVENT_CONFIG = {
   childName: "Clara",
-  eventDate: "2026-06-20T15:00:00",
-  eventTime: "15:00",
-  address: "Petrópolis, RJ",
-  mapsLink: "https://maps.google.com/",
+  eventDate: "2026-06-20T13:00:00",
+  eventTime: "13:00",
+  address: "Sítio Nossa Senhora de Fátima, R. Neuza Goulart Brizola, 1959 - Itaipava, Petrópolis - RJ, 25750-037",
+  mapsLink:
+    "https://www.google.com/maps/place/R.+Neuza+Goulart+Brizola,+1959+-+Itaipava,+Petr%C3%B3polis+-+RJ,+25750-037/@-22.3479247,-43.1085306,17z/data=!3m1!4b1!4m6!3m5!1s0x98549038f2c499:0x20886669dda6f367!8m2!3d-22.3479247!4d-43.1085306!16s%2Fg%2F11fst7xvcm!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDQwMS4wIKXMDSoASAFQAw%3D%3D",
   whatsappNumber: "21997914496",
   adminPassword: "admin123",
   siteUrl: window.location.origin,
