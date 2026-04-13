000f666318eaae0d138951b9611f3ccfb57a4ef4
Nerdwork <nerdworktecnologia@gmail.com>
2026-04-13 00:06:26 -0300

    fix: envia confirmacao do RSVP para whatsapp dos EUA

--- src/lib/eventConfig.ts
+++ src/lib/eventConfig.ts
@@ -8,7 +8,7 @@ export const EVENT_CONFIG = {
   address: "Sítio Nossa Senhora de Fátima, R. Neuza Goulart Brizola, 1959 - Itaipava, Petrópolis - RJ, 25750-037",
   mapsLink:
     "https://www.google.com/maps/place/R.+Neuza+Goulart+Brizola,+1959+-+Itaipava,+Petr%C3%B3polis+-+RJ,+25750-037/@-22.3479247,-43.1085306,17z/data=!3m1!4b1!4m6!3m5!1s0x98549038f2c499:0x20886669dda6f367!8m2!3d-22.3479247!4d-43.1085306!16s%2Fg%2F11fst7xvcm!18m1!1e1?entry=ttu&g_ep=EgoyMDI2MDQwMS4wIKXMDSoASAFQAw%3D%3D",
-  whatsappNumber: "21997914496",
+  whatsappNumber: "19173091832",
   adminPassword: "admin123",
   siteUrl: window.location.origin,
 };
